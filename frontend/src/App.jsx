cc34980df9c490da486b4c4d65fafd7507d02154
Veysel Doğan <veyseldogann0@gmail.com>
2026-03-17 08:38:35 +0300

    feat: setup react frontend with tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white text-center p-10">
+      <h1 className="text-4xl font-bold">Tailwind Çalışıyor!</h1>
+    </div>
+  )
+}
+
+export default App
